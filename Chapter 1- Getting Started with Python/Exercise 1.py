771ac246be922cbeb6ca17a8338ed5e6ab92c3be
rania3104 <raniaazizfarooqi.14574.11g@gmail.com>
2022-10-12 09:45:32 +0400

    Create Exercise 1.py

---
+++ Chapter 1- Getting Started with Python/Exercise 1.py
@@ -0,0 +1,7 @@
+print("""Twinkle, twinkle, little star,
+\t How I wonder what you are!
+\t \t Up above the world so high,
+\t \t Like a diamond in the sky.
+Twinkle, twinkle, little star,
+\t How I wonder what you are
+""")
