5b377dae188c53b51164ab427e9cea3fa4cdb289
rania3104 <raniaazizfarooqi.14574.11g@gmail.com>
2022-10-30 21:43:30 +0400

    finished Chapter 3

--- Chapter 3- Data Structures/Exercise 1.py
+++ Chapter 3- Data Structures/Exercise 1.py
@@ -1,5 +1,5 @@
 #Exercise 1
-names=["Rumaisa","Ayesha","Arwa","Maryam","Fatima","Shanzay"]
+names=["Rumaisa","Ayesha","Arwa","Maryam","Fatimah","Shanzay"]
 print(names[0])
 print(names[1])
 print(names[2])
