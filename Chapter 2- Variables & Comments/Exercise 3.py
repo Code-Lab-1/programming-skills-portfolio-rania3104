0ba280bbc67d9b1d3d84b7d63a7edaf74f8dbeb8
rania3104 <raniaazizfarooqi.14574.11g@gmail.com>
2022-10-30 14:59:30 +0400

    finished Chapter 2

--- Chapter 2- Variables & Comments/Exercise 3.py
+++ Chapter 2- Variables & Comments/Exercise 3.py
@@ -4,7 +4,7 @@ Message = '\n\t"You need to buy the groceries,\n\tand make sure you do it before
 print(Name.title(), Message)
 
 Nameagain = " Isaac Newton "
-print(Nameagain)
-print(Nameagain.lstrip())
-print(Nameagain.rstrip())
-print(Nameagain.strip())
+print("Original","\n"+Nameagain)
+print("lstrip","\n"+Nameagain.lstrip())
+print("rstrip","\n"+Nameagain.rstrip())
+print("strip","\n"+Nameagain.strip())
