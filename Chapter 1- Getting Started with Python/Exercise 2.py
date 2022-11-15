8f1309650105a0c3dc1b2596fd60cf89b129372e
rania3104 <raniaazizfarooqi.14574.11g@gmail.com>
2022-11-15 17:00:21 +0400

    finished Chapter 1

--- Chapter 1- Getting Started with Python/Exercise 2.py
+++ Chapter 1- Getting Started with Python/Exercise 2.py
@@ -1,3 +1,4 @@
 #Exercise 2
 import sys
+
 print("Python version: ", sys.version)
--- Chapter 1- Getting Started with Python/Exercise 3.py
+++ Chapter 1- Getting Started with Python/Exercise 3.py
@@ -1,4 +1,4 @@
 #Exercise 3
 import datetime
 current=datetime.datetime.now()
-print("The current date and time is:\n", current.strftime("%d-%m-%Y \n%H:%M:%S"))
+print("The current date and time is:\n"+current.strftime("%d-%m-%Y \n%H:%M:%S"))
