cf2890395bdd1c222dcc3779b7edcc3f000c867c
rania3104 <raniaazizfarooqi.14574.11g@gmail.com>
2022-11-21 23:05:27 +0400

    finished all exercises

---
+++ Chapter 5- Dictionaries/Exercise 7 (extra).py
@@ -0,0 +1,10 @@
+#Exercise 7 (extra)
+details = {
+    "Name": "Rania",
+    "Age": 18,
+    "Class": "Creative Computing",
+    "Gender": "Female"
+}
+
+details['Course'] = details.pop('Class')
+print(details)
